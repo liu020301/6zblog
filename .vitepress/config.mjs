423d9ab65356c758ae1636c50274dae75dc2d9fd
Zehao Liu <liuzehaobarry@gmail.com>
2025-03-19 00:42:01 -0400

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,66 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from './utils/auto_sidebar.mjs'
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "6z Blog",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    outlineTitle: '目录',
+    outline: [1, 6],
+    logo: '/blog_icon.svg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', items:[
+        { text: 'Markdown Examples', link: '/markdown-examples' },
+        { text: 'Runtime API Examples', link: '/api-examples' }
+      ] },
+      { text: "碎碎念", link: '/life'},
+      { text: "关于我", link: "/about-me"},
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+
+    sidebar: {
+      "/life": set_sidebar("/life"),
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/liu020301' }
+    ],
+
+    footer: {
+      copyright: "Copyright@ 2025 Zehao Liu",
+    },
+    
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+
+  }
+})
---
+++ index.md
@@ -0,0 +1,22 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "6z Blog"
+  text: "网络自留地"
+  tagline: 纸上得来终觉浅，绝知此事要躬行。
+  actions:
+    - theme: brand
+      text: 为何建站
+      link: /life/why
+
+# features:
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
